40d94df984c73b96fbe61b7db94be56db8c263e7
Garciaj007 <jurielgarcia2010@gmail.com>
2020-04-27 01:58:27 -0400

    Configured Webpack, And Installed Dependancies

--- _posts/2020-04-24-MakingWebsite.md
+++ _posts/2020-04-24-MakingWebsite.md
@@ -1,6 +1,7 @@
 --
 layout: post
 title: "Making a website with Jekyll, Three.js, Tone.js & anime.js"
+date: 2020-04-24
 --
 
 ## Fuck me in the pussy
---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+const path = require('path');
+
+module.exports = {
+    entry: "./webpack/entry.js",
+    output: {
+        path: path.resolve(__dirname, 'assets/js/'),
+        filename: "bundle.js"
+    }
+};
